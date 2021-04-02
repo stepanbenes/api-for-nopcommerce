d35fd82d3ccc6e4f1ee231bbdc6e602d40d26192
Štěpán Beneš <benes.stepan@gmail.com>
2021-04-02 16:14:16 +0200

    Call api from ClientApp

--- ClientApp/Program.cs
+++ ClientApp/Program.cs
@@ -1,17 +1,32 @@
 ﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ApiBindings.NopApi;
 
 namespace ClientApp
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static async Task Main(string[] args)
 		{
-			Console.WriteLine("Types in this assembly:");
-			foreach (Type t in typeof(Program).Assembly.GetTypes())
+			//Console.WriteLine("Types in this assembly:");
+			//foreach (Type t in typeof(Program).Assembly.GetTypes())
+			//{
+			//	Console.WriteLine(t.FullName);
+			//}
+			//Console.ReadKey();
+
+			HttpClient httpClient = new HttpClient { BaseAddress = new Uri("https://localhost:5001") };
+			NopApiClient nopApiClient = new NopApiClient(httpClient);
+			var categories = await nopApiClient.GetApiCategories();
+			if (categories?.Categories is not null)
 			{
-				Console.WriteLine(t.FullName);
+				foreach (var category in categories.Categories)
+				{
+					Console.WriteLine(category.ToString());
+				}
 			}
-			Console.ReadKey();
 		}
 	}
 }
