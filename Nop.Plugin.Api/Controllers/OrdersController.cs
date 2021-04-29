6630b947c26a1ab2066a9b52e482934d6cb49b29
Štěpán Beneš <benes.stepan@gmail.com>
2021-04-29 15:32:53 +0200

    Revert address id validation in CreateOrder method

--- Nop.Plugin.Api/Controllers/OrdersController.cs
+++ Nop.Plugin.Api/Controllers/OrdersController.cs
@@ -284,18 +284,10 @@ namespace Nop.Plugin.Api.Controllers
 			{
 				return Error(HttpStatusCode.BadRequest, "billingAddress", "missing billing address");
 			}
-			if (orderDelta.Dto.BillingAddress.Id == 0)
-			{
-				return Error(HttpStatusCode.BadRequest, "billingAddress", "non-existing billing address");
-			}
 
 			if (orderDelta.Dto.ShippingAddress == null)
 			{
 				return Error(HttpStatusCode.BadRequest, "shippingAddress", "missing shipping address");
-			}
-			if (orderDelta.Dto.ShippingAddress.Id == 0)
-			{
-				return Error(HttpStatusCode.BadRequest, "shippingAddress", "non-existing shipping address");
 			}
 
 			if (!await CheckPermissions(orderDelta.Dto.CustomerId))
